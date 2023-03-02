7ba73e1ca595135f961e59f6ed42b5e3a6166a9a
Nicholas Wong <nicholas.wong01@ad.infosys.com>
2023-03-02 10:51:36 -0800

    remove unnecessary spring dependencies

--- jenkins/src/main/java/com/demo/jenkins/JenkinsApplication.java
+++ jenkins/src/main/java/com/demo/jenkins/JenkinsApplication.java
@@ -1,13 +1,10 @@
 package com.demo.jenkins;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
 public class JenkinsApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(JenkinsApplication.class, args);
+		System.out.println("hello world");
 	}
 
 }
