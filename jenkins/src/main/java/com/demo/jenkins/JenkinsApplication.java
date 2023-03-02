614fea7bc2e63b953e8664d42b3fa84c674459e9
Nicholas Wong <nicholas.wong01@ad.infosys.com>
2023-03-02 12:01:28 -0800

    revert to springboot app

--- jenkins/src/main/java/com/demo/jenkins/JenkinsApplication.java
+++ jenkins/src/main/java/com/demo/jenkins/JenkinsApplication.java
@@ -1,10 +1,13 @@
 package com.demo.jenkins;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class JenkinsApplication {
 
 	public static void main(String[] args) {
-		System.out.println("hello world");
+		SpringApplication.run(JenkinsApplication.class, args);
 	}
 
 }
